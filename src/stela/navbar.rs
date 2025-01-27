e9f514053dc1cac40deba148311377c290a65956
Brandon Dyer <brandondyer64@gmail.com>
2025-01-27 16:45:25 -0700

    Navbar

---
+++ src/stela/navbar.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+use crate::stela::{Image, Motion, VisualMotion};
+
+/// Representation of data and buttons on the top/side navbars.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Navbar {
+    /// Motion for when left icon image is selected.
+    pub left_side_motion: Motion,
+    /// Primary image to show on left side of navbar.
+    pub left_side_icon_image: Image,
+    /// Dim text to show next to primary icon image.
+    pub left_side_secondary_text: Option<String>,
+    /// Motion for when search bar is clicked.
+    pub search_motion: Option<Motion>,
+    /// Buttons on right side of navbar.
+    pub right_side_motions: Vec<VisualMotion>,
+    /// Buttons in side nav.
+    pub side_motions: Vec<VisualMotion>,
+}
